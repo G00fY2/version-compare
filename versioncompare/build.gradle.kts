6979725dc918cd9e6524eb321424b143d8fb83d6
Thomas Wirth <thomas-w-@web.de>
2021-03-22 01:09:53 +0100

    Fix sonatype repository url

--- versioncompare/build.gradle.kts
+++ versioncompare/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
     repositories {
         maven {
             name = "sonatype"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = project.findStringProperty("sonatypeUsername")
                 password = project.findStringProperty("sonatypePassword")
