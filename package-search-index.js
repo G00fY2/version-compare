4355118922f5767d69404bb23dcd5454a1f016e6
Thomas Wirth <thomas-w-@web.de>
2022-01-02 15:49:12 +0100

    Update javadoc

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.g00fy2.versioncompare"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.g00fy2.versioncompare","l":"Version"}];updateSearchResults();
